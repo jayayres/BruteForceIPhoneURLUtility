f018ee52e211a76ad2e8476b5f111fd25a9c1f68
Jay Ayres <jayayres@gmail.com>
2012-04-29 21:30:25 -0400

    Add compressed BFS algorithm

---
+++ BruteForceCanOpenURL/NSDataAdditions.h
@@ -0,0 +1,13 @@
+//
+//  LFCGzipUtility.h
+//  URL Utility
+//
+//  Created by Jay Ayres on 3/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LFCGzipUtility : NSObject
+
+@end
---
+++ BruteForceCanOpenURL/NSDataAdditions.m
@@ -0,0 +1,13 @@
+//
+//  LFCGzipUtility.m
+//  URL Utility
+//
+//  Created by Jay Ayres on 3/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "LFCGzipUtility.h"
+
+@implementation LFCGzipUtility
+
+@end
